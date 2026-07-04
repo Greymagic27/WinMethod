32515980165b8c99e7d7dbde0149255549302e47
Greymagic27 <tolly67p@gmail.com>
2026-07-04 18:48:41 +0100

    Remove them as redundant

--- src/test/java/io/github/greymagic27/jna_clone/HandleTest.java
+++ src/test/java/io/github/greymagic27/jna_clone/HandleTest.java
@@ -6,7 +6,6 @@ import java.lang.foreign.MemorySegment;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class HandleTest {
 
@@ -32,38 +31,5 @@ class HandleTest {
         HANDLE fromLong = new HANDLE(0x1234);
         HANDLE fromSegment = new HANDLE(MemorySegment.ofAddress(0x1234));
         assertEquals(fromSegment.segment().address(), fromLong.segment().address());
-    }
-
-    @Test
-    void testLongConstructor_Zero() {
-        HANDLE handle = new HANDLE(0);
-        assertEquals(0, handle.segment().address());
-    }
-
-    @Test
-    void testLongConstructor_Negative() {
-        HANDLE handle = new HANDLE(-999);
-        HANDLE handle2 = new HANDLE(-12938);
-        assertEquals(-999, handle.segment().address());
-        assertEquals(-12938, handle2.segment().address());
-        assertNotEquals(handle, handle2);
-    }
-
-    @Test
-    void testLongConstructor_Positive() {
-        HANDLE handle = new HANDLE(9999999);
-        HANDLE handle2 = new HANDLE(11280482);
-        assertEquals(9999999, handle.segment().address());
-        assertEquals(11280482, handle2.segment().address());
-        assertNotEquals(handle, handle2);
-    }
-
-    @Test
-    void testLongConstructor_DecimalPlace() {
-        HANDLE handle = new HANDLE((long) 9.99998);
-        HANDLE handle2 = new HANDLE((long) 1.30492);
-        assertEquals(9.0, handle.segment().address());
-        assertEquals(1.0, handle2.segment().address());
-        assertNotEquals(handle, handle2);
     }
 }
