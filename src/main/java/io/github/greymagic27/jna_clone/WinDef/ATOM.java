a9783dad0e2e821813aeeb41124354e353366c4c
Greymagic27 <tolly67p@gmail.com>
2026-07-13 10:46:10 +0100

    Add missing javadoc for ATOM

--- src/main/java/io/github/greymagic27/jna_clone/WinDef/ATOM.java
+++ src/main/java/io/github/greymagic27/jna_clone/WinDef/ATOM.java
@@ -1,5 +1,8 @@
 package io.github.greymagic27.jna_clone.WinDef;
 
+/**
+ * An atom. For more information, see <a href="https://learn.microsoft.com/en-us/windows/desktop/dataxchg/about-atom-tables">About Atom Tables</a>
+ */
 public class ATOM extends WORD {
     /**
      * @param value A {@link Short} value
