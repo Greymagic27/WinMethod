cfe70c6303c7cf2ffe659ba2b6c62eb1261bac55
Greymagic27 <tolly67p@gmail.com>
2026-07-28 18:09:50 +0100

    Add array mappings. Closes #9

--- src/main/java/io/github/greymagic27/win_method/TypeMapper.java
+++ src/main/java/io/github/greymagic27/win_method/TypeMapper.java
@@ -17,17 +17,18 @@ import org.jspecify.annotations.Nullable;
 public final class TypeMapper {
 
     static @Nullable MemoryLayout layoutMappings(Class<?> javaType) {
-        if (javaType == int.class || javaType == Integer.class || javaType == LONG.class) return ValueLayout.JAVA_INT;
+        if (javaType == int.class || javaType == int[].class || javaType == Integer.class || javaType == LONG.class) return ValueLayout.JAVA_INT;
         if (javaType == boolean.class || javaType == Boolean.class || javaType == BOOL.class) return ValueLayout.JAVA_INT;
         if (javaType == long.class || javaType == Long.class || javaType == LRESULT.class || javaType == LPARAM.class || javaType == WPARAM.class) return ValueLayout.JAVA_LONG;
         if (javaType == short.class || javaType == Short.class) return ValueLayout.JAVA_SHORT;
-        if (javaType == byte.class || javaType == Byte.class || javaType == BYTE.class) return ValueLayout.JAVA_BYTE;
+        if (javaType == byte.class || javaType == byte[].class || javaType == Byte.class || javaType == BYTE.class) return ValueLayout.JAVA_BYTE;
         if (javaType == double.class || javaType == Double.class) return ValueLayout.JAVA_DOUBLE;
         if (javaType == float.class || javaType == Float.class) return ValueLayout.JAVA_FLOAT;
+        if (javaType == char.class || javaType == char[].class) return ValueLayout.JAVA_CHAR;
         if (javaType == String.class) return ValueLayout.ADDRESS;
+        if (javaType == void.class || javaType == Void.class) return null;
         if (Structure.class.isAssignableFrom(javaType) || Callback.class.isAssignableFrom(javaType) || Pointer.class.isAssignableFrom(javaType)) return ValueLayout.ADDRESS;
         if (WORD.class.isAssignableFrom(javaType)) return ValueLayout.JAVA_SHORT;
-        if (javaType == void.class || javaType == Void.class) return null;
         throw new IllegalArgumentException("No native layout mapping for: " + javaType);
     }
 
