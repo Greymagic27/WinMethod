0f8642f84b030722802f4809df33d6443bbafa08
Greymagic27 <tolly67p@gmail.com>
2026-07-31 18:59:32 +0100

    Update javadoc

--- src/main/java/io/github/greymagic27/win_method/WinDef/HKEY.java
+++ src/main/java/io/github/greymagic27/win_method/WinDef/HKEY.java
@@ -3,6 +3,7 @@ package io.github.greymagic27.win_method.WinDef;
 import io.github.greymagic27.win_method.WinNT.HANDLE;
 import java.lang.foreign.MemorySegment;
 
+/// A {@link HANDLE} to a registry key
 public class HKEY extends HANDLE {
     public HKEY(MemorySegment segment) {
         super(segment);
