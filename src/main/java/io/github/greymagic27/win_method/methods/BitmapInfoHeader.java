21e27aaa9d429b16cdbf2ccae44c6e66835d9cc3
Greymagic27 <tolly67p@gmail.com>
2026-08-05 18:57:06 +0100

    Change BitmapInfoHeader slightly

--- src/main/java/io/github/greymagic27/win_method/methods/BitmapInfoHeader.java
+++ src/main/java/io/github/greymagic27/win_method/methods/BitmapInfoHeader.java
@@ -3,6 +3,7 @@ package io.github.greymagic27.win_method.methods;
 import io.github.greymagic27.win_method.IntSafe.DWORD;
 import io.github.greymagic27.win_method.WinDef.WORD;
 import io.github.greymagic27.win_method.WinNT.LONG;
+import io.github.greymagic27.win_method.types.WinGdi.BITMAPINFO;
 import io.github.greymagic27.win_method.types.WinGdi.BITMAPINFOHEADER;
 import org.jspecify.annotations.NonNull;
 
@@ -11,7 +12,7 @@ public class BitmapInfoHeader {
 
     /// Creates a {@link BITMAPINFOHEADER} that contains information about the dimension and colour format of a device-independent bitmap (DIB)
     ///
-    /// @param size          Specifies the number of bytes required by the structure. This does not include the size of the colour table or the size of the colour masks
+    /// @param bitmapInfo    The bitmap information structure to configure
     /// @param width         Specifies the width of the bitmap, in pixels
     /// @param height        Specifies the height of the bitmap, in pixels
     /// @param planes        Specifies the number of planes for the target device. This value must be 1
@@ -22,10 +23,9 @@ public class BitmapInfoHeader {
     /// @param yPelsPerMeter Specifies the vertical resolution, in pixels per meter, of the target device for the bitmap
     /// @param clrUsed       Specifies the number of colour indices in the colour table that are actually used by the bitmap
     /// @param clrImportant  Specifies the number of colour indices that are considered important for displaying the bitmap. If this value is zero, all colours are important
-    /// @return Returns the populated {@link BITMAPINFOHEADER}
-    public static @NonNull BITMAPINFOHEADER createBitmapInfoHeader(int size, int width, int height, int planes, int bitCount, int compression, int sizeImage, int xPelsPerMeter, int yPelsPerMeter, int clrUsed, int clrImportant) {
-        BITMAPINFOHEADER header = new BITMAPINFOHEADER();
-        header.biSize = new DWORD(size);
+    public static void setBitmapInfoHeader(@NonNull BITMAPINFO bitmapInfo, int width, int height, int planes, int bitCount, int compression, int sizeImage, int xPelsPerMeter, int yPelsPerMeter, int clrUsed, int clrImportant) {
+        BITMAPINFOHEADER header = bitmapInfo.bmiHeader;
+        header.biSize = new DWORD(header.size());
         header.biWidth = new LONG(width);
         header.biHeight = new LONG(height);
         header.biPlanes = new WORD((short) planes);
@@ -36,6 +36,5 @@ public class BitmapInfoHeader {
         header.biYPelsPerMeter = new LONG(yPelsPerMeter);
         header.biClrUsed = new DWORD(clrUsed);
         header.biClrImportant = new DWORD(clrImportant);
-        return header;
     }
 }
--- src/test/java/io/github/greymagic27/win_method/methods/BitmapInfoHeaderTest.java
+++ src/test/java/io/github/greymagic27/win_method/methods/BitmapInfoHeaderTest.java
@@ -1,6 +1,8 @@
 package io.github.greymagic27.win_method.methods;
 
+import io.github.greymagic27.win_method.types.WinGdi;
 import io.github.greymagic27.win_method.types.WinGdi.BITMAPINFOHEADER;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,9 +10,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class BitmapInfoHeaderTest {
 
+    private WinGdi.BITMAPINFO bitmapinfo;
+
+    @BeforeEach
+    void setUp() {
+        bitmapinfo = new WinGdi.BITMAPINFO();
+    }
+
     @Test
-    void testCreateBitmapInfoHeader() {
-        BITMAPINFOHEADER header = BitmapInfoHeader.createBitmapInfoHeader(40, 1920, 1080, 1, 32, 0, 1920 * 1080 * 4, 3780, 3780, 0, 0);
+    void testSetBitmapInfoHeader() {
+        BitmapInfoHeader.setBitmapInfoHeader(bitmapinfo, 1920, 1080, 1, 32, 0, 1920 * 1080 * 4, 3780, 3780, 0, 0);
+        BITMAPINFOHEADER header = bitmapinfo.bmiHeader;
         assertNotNull(header);
         assertEquals(40, header.biSize.intValue());
         assertEquals(1920, header.biWidth.intValue());
@@ -26,10 +36,11 @@ class BitmapInfoHeaderTest {
     }
 
     @Test
-    void testCreateBitmapInfoHeaderAllZeroValues() {
-        BITMAPINFOHEADER header = BitmapInfoHeader.createBitmapInfoHeader(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    void testSetBitmapInfoHeaderWithZeroValues() {
+        BitmapInfoHeader.setBitmapInfoHeader(bitmapinfo, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        BITMAPINFOHEADER header = bitmapinfo.bmiHeader;
         assertNotNull(header);
-        assertEquals(0, header.biSize.intValue());
+        assertEquals(40, header.biSize.intValue());
         assertEquals(0, header.biWidth.intValue());
         assertEquals(0, header.biHeight.intValue());
         assertEquals(0, header.biPlanes.shortValue());
